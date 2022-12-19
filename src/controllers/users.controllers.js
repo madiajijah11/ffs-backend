0472cd8000edcdee563e96a4d5cb86a6f201a56e
MexL <madiajijah7@gmail.com>
2022-12-19 10:50:20 +0800

    remove hash from users controller, will be used at register

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -22,7 +22,6 @@ exports.getUsers = async (req, res) => {
 };
 exports.createUsers = async (req, res) => {
   try {
-    req.body.password = await bcrypt.hash(req.body.password, 10);
     const users = await createUser(req.body);
     res.status(200).json({
       success: true,
@@ -48,10 +47,6 @@ exports.getUserById = async (req, res) => {
 };
 
 exports.updateUser = async (req, res) => {
-  const { password } = req.body;
-  if (password) {
-    req.body.password = await bcrypt.hash(password, 10);
-  }
   try {
     const user = await updateUser(req.params.id, req.body);
     res.status(200).json({
