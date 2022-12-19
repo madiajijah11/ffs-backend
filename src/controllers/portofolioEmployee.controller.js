f9b89eb3e2a57ab5a7a3f4e4cdbee1038812df5f
mstkm <99125333+mstkm@users.noreply.github.com>
2022-12-19 16:32:40 +0700

    add CRUD Portofolio Employee

---
+++ src/controllers/portofolioEmployee.controller.js
@@ -0,0 +1,72 @@
+const {
+  getAllPortofolioEmployee,
+  createPortofolioEmployee,
+  getPortofolioEmployeById,
+  updatePortofolioEmployee,
+  deletePortofolioEmployee} = require('../models/portofolioEmployee.model')
+const errorHandler = require("../helpers/errorHandler.helper");
+
+exports.getAllPortofolioEmployee = async (req, res) => {
+  try {
+    const allPortofolioEmployee = await getAllPortofolioEmployee();
+    res.status(200).json({
+      success: true,
+      message: "All Portofolio Employee retrieved successfully",
+      results: allPortofolioEmployee,
+    });
+  } catch (error) {
+    if (error) return errorHandler(error, res);
+  }
+};
+
+exports.createPortofolioEmployee = async (req, res) => {
+  try {
+    const users = await createPortofolioEmployee(req.body);
+    res.status(200).json({
+      success: true,
+      message: "Portofolio employee created successfully",
+      results: users,
+    });
+  } catch (error) {
+    if (error) return errorHandler(error, res);
+  }
+};
+
+exports.getPortofolioEmployeeById = async (req, res) => {
+  try {
+    const portofolioEmployee = await getPortofolioEmployeById(req.params.id);
+    res.status(200).json({
+      success: true,
+      message: "Portofolio employee retrieved successfully",
+      results: portofolioEmployee,
+    });
+  } catch (error) {
+    if (error) return errorHandler(error, res);
+  }
+};
+
+exports.updatePortofolioEmployee = async (req, res) => {
+  try {
+    const portofolioEmployee = await updatePortofolioEmployee(req.params.id, req.body);
+    res.status(200).json({
+      success: true,
+      message: "Portofolio employee updated successfully",
+      results: portofolioEmployee,
+    });
+  } catch (error) {
+    if (error) return errorHandler(error, res);
+  }
+};
+
+exports.deletePortofolioEmployee = async (req, res) => {
+  try {
+    const portofolioEmployee = await deletePortofolioEmployee(req.params.id);
+    res.status(200).json({
+      success: true,
+      message: "Portofolio employee deleted successfully",
+      results: portofolioEmployee,
+    });
+  } catch (error) {
+    if (error) return errorHandler(error, res);
+  }
+};
--- src/routers/index.js
+++ src/routers/index.js
@@ -7,6 +7,7 @@ routers.use("/groupUsers", require("./groupUsers.router"));
 routers.use("/skill", require("./skill.router"));
 routers.use("/profileCompany", require("./profileCompany.router"));
 routers.use("/employeeSkill", require("./skillEmployee.router"));
+routers.use("/portofolioEmployee", require("./portofolioEmployee.router"));
 
 
 module.exports = routers;
