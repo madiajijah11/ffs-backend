b1b39af9e0fe6e874e23dd1fd38dc44fea968ec4
MexL <madiajijah7@gmail.com>
2023-02-01 18:02:57 +0800

    add picture field on getEmployeeLists model

--- src/models/employeeLists.model.js
+++ src/models/employeeLists.model.js
@@ -1,6 +1,6 @@
 const dbHelper = require('../helpers/db.helper')
 
-exports.countAllEmployeeLists = async (filter) => {
+exports.countAllEmployeeLists = async filter => {
   try {
     const sql = `SELECT COUNT (u.id) as "totalData"
     FROM users u
@@ -17,10 +17,11 @@ exports.countAllEmployeeLists = async (filter) => {
   }
 }
 
-exports.getEmployeeLists = async (filter) => {
+exports.getEmployeeLists = async filter => {
   try {
     const sql = `SELECT u.id,
     u."fullName",
+    u.picture
     pE."jobDesk",
     wT.name as "workTime",
     pE.domicile,
@@ -33,7 +34,7 @@ exports.getEmployeeLists = async (filter) => {
     FULL JOIN "employeeSkill" eS on u.id = eS."userId"
     FULL JOIN skills s on s.id = eS."skillId"
     WHERE u."groupUser" = '1' AND s.name LIKE $1
-    GROUP BY u.id, u."fullName", pE."jobDesk", wT.name, pE.domicile, u."createdAt", u."updatedAt"
+    GROUP BY u.id, u."fullName", u.picture, pE."jobDesk", wT.name, pE.domicile, u."createdAt", u."updatedAt"
     ORDER BY "${filter.sortBy}" ${filter.sort} LIMIT $2 OFFSET $3`
     const values = [`%${filter.search}%`, filter.limit, filter.offset]
     const result = await dbHelper.query(sql, values)
